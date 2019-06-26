231f2b73d4f46567c93216a75339c13eee5721be
Chris Anastasi <stachedata@gmail.com>
2019-06-25 22:01:47 -0400

    created join and host component

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './app.css'
 
-class App extends React.Component {
+class Join extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>
+    return <h1>Hello Join</h1>
   }
 }
 
-ReactDOM.render(<App name="World" />, document.getElementById('root'))
+class Host extends React.Component {
+  render() {
+    return <h1>Hello Host</h1>
+  }
+}
+
+class App extends React.Component {
+  clickJoin() {
+    ReactDOM.render(<Join />, document.getElementById('root'))
+  }
+
+  clickHost() {
+    ReactDOM.render(<Host />, document.getElementById('root'))
+  }
+
+  render() {
+    return (
+      <div id="buttonsContainer">
+        <button onClick={this.clickJoin}>Join</button>
+        <button onClick={this.clickHost}>Host</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

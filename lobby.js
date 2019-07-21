2aa2319b5546dcae852122e2cb7565912ee55592
Chris Anastasi <stachedata@gmail.com>
2019-07-21 18:05:25 -0400

    created team components. added user joining logic

--- lobby.js
+++ lobby.js
@@ -1,11 +1,45 @@
 import React from 'react'
 
+class Judge extends React.Component {
+  render() {
+    return (
+      <div id="judge" className="teams">
+        <p className="teamTitle">Judge</p>
+        <p>{this.props.player}</p>
+      </div>
+    )
+  }
+}
+
+class Team extends React.Component {
+  componentDidMount() {
+    console.log('mount: ', this.props.players)
+  }
+  componentDidUpdate() {
+    console.log('update: ', this.props.players)
+  }
+
+  render() {
+    return (
+      <div id="team" className="teams">
+        <p className="teamTitle">Team {this.props.name}</p>
+        <p>{this.props.players}</p>
+      </div>
+    )
+  }
+}
+
 class Lobby extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: '',
-      players: '',
+      players: [''],
+    }
+
+    this.team = {
+      counter: 1,
+      a: [''],
+      b: [''],
     }
   }
 
@@ -14,25 +48,28 @@ class Lobby extends React.Component {
     this.props.room.emit('join', name, rsp => {
       this.setState({ players: rsp })
     })
+    // TODO fix this up
+    // this.updater = setInterval(() => {
+    //   this.props.room.emit('users', rspo => this.setState({ players: rspo }))
+    // }, 1000)
   }
 
   componentDidUpdate() {
-    console.log('players: ', Object.values(this.state.players))
+    if (this.team.counter % 2 == 0) {
+      this.team.b.push(this.state.players[this.team.counter])
+    } else {
+      this.team.a.push(this.state.players[this.team.counter])
+    }
+    console.log('Lobby update: ', this.state.players)
   }
 
   render() {
     return (
       <div id="hostScreen">
         <h1 id="roomNumber">Room #{this.props.roomNumber}</h1>
-        <div id="judge" className="teams">
-          <p className="teamTitle">Judge</p>
-        </div>
-        <div id="team1" className="teams">
-          <p className="teamTitle">Team 1</p>
-        </div>
-        <div id="team2" className="teams">
-          <p className="teamTitle">Team 2</p>
-        </div>
+        <Judge player={Object.values(this.state.players[0])} />
+        <Team name={'A'} players={Object.values(this.team.a)} />
+        <Team name={'B'} players={Object.values(this.team.b)} />
         <button>Ready</button>
       </div>
     )
